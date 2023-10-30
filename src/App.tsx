042666bc8b46362baa90ec724047299d8b185e36
Nigel Wilson <nigel@stemgarden.org>
2023-10-30 11:23:56 -0500

    Update App.tsx
    Testing new navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { AppContextProvider } from "./context/AppContext";
+
+import { BrowserRouter } from "react-router-dom"
+
+
+// COMPONENTS
+import Navbar from "./components/navigation/Navbar"
+
+// MUI
+import theme from "./theme"
+import { ThemeProvider } from '@mui/material/styles';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import stemGardenLogoWithBG from '/sg-logo-192px.png'
+import stemGardenLogo from '/sg-logo-transparent-bg.png'
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+      <AppContextProvider >
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+
+            <Navbar />
+            <div>
+              <a href="https://vitejs.dev" target="_blank">
+                <img src={stemGardenLogo} className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo spinning" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+            </BrowserRouter>
+        </ThemeProvider>
+      </AppContextProvider>
     </>
   )
 }
 
-export default App
