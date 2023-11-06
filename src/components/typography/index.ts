30e62ed11e404cdcb39971721b55424738e967ae
Nigel Wilson <nigel@stemgarden.org>
2023-11-05 18:34:19 -0600

    Create index of custom typography components

---
+++ src/components/typography/index.ts
@@ -0,0 +1,16 @@
+
+
+// export {default as Paragraph} from './Paragraph';
+// export {default as PageHeader} from './PageHeader';
+export {default as Title} from './Title';
+export {default as Subtitle} from './Subtitle';
+export {default as Heading} from './Heading';
+// export {default as ButtonLabel} from './ButtonLabel';
+
+
+// export {default as Section} from './Section';
+// export {default as Quote} from './Quote';
+// export {default as PictureRow} from './PictureRow';
+// export {default as Testimony} from './Testimony';
+
+
