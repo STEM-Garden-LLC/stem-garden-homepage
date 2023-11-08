4efc4cb6d2830a3d240fff3d4a6ac915ea37c9c7
Nigel Wilson <nigel@stemgarden.org>
2023-11-07 18:27:12 -0600

    Set maxWidth

--- src/components/navigation/ButtonWithIcon.tsx
+++ src/components/navigation/ButtonWithIcon.tsx
@@ -2,26 +2,18 @@
 import { Box, Button } from '@mui/material';
 
 // Font Awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconDefinition, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
 
 
 // CUSTOM COMPONENTS
 import { ButtonLabel } from '../typography';
-import PictureRow from '../components/PictureRow';
-// import WhatWeGrowSection from './WhatWeGrowSection';
-// import { Footer, ScrollToTopButton } from '../../components/navigation';
-
-
 
 type ButtonWithIconProps = {
   text: string;
-  textColor: string;
   href: string;
+  textColor?: string;
   fontFamily?: string;
   align?: string;
-  // startIcon?: IconProps;
-  // endIcon?: IconProps;
   startIcon?: IconDefinition;
   endIcon?: IconDefinition;
   // iconSize?: string;
@@ -30,16 +22,10 @@ type ButtonWithIconProps = {
 export default function ButtonWithIcon(props: ButtonWithIconProps) {
   const { 
     text,
-    textColor, 
     href,
-    fontFamily = 'roboto', 
-    align = 'center',
     startIcon,
     endIcon, 
   } = props
-
-  const iconSize = 'lg'
-  const iconBgColor = "darkGrey" 
 
 
   return (
@@ -49,7 +35,9 @@ export default function ButtonWithIcon(props: ButtonWithIconProps) {
       sx={{ 
         color: "white", 
         border: "solid white 1px", 
-        marginY: 1,  
+        maxWidth: '400px',  
+        marginX: 'auto',  
+        marginY: 2,  
         padding: 1,
         width: '100%',
         display: 'flex',
@@ -57,38 +45,11 @@ export default function ButtonWithIcon(props: ButtonWithIconProps) {
 
       }} 
     >
-      {
-        startIcon ? (
-          <Box paddingX={2} >
-            <FontAwesomeIcon icon={startIcon} color={textColor} size={iconSize} />
-          </Box>
-        ) : (
-          <></>
-        )
-      }
-      <ButtonLabel text={text} textColor={textColor} />
-      {
-        endIcon ? (
-          <Box paddingX={2} >
-            <FontAwesomeIcon icon={endIcon} color={textColor} size={iconSize} />
-          </Box>
-        ) : (
-          <></>
-        )
-      }
+      <ButtonLabel 
+        text={text} 
+        startIcon={startIcon}
+        endIcon={endIcon} 
+      />
     </Button>
   )
 }
-
-
-// <Button 
-          //   href="contact-us" 
-          //   variant="outlined"
-          //   sx={{ color: "white", border: "solid white 1px", margin: 1  }} 
-          //   children={
-          //     <ButtonLabel 
-          //       text="Contact Us" 
-          //       endIcon={faArrowCircleRight} 
-          //     />
-          //   } 
-          // />
