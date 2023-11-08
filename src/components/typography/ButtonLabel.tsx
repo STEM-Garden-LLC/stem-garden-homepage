97c2ec87895179802f2833678593f7deacc78025
Nigel Wilson <nigel@stemgarden.org>
2023-11-07 18:27:37 -0600

    Integrate icons with button label component

--- src/components/typography/ButtonLabel.tsx
+++ src/components/typography/ButtonLabel.tsx
@@ -1,33 +1,77 @@
 import { useContext } from 'react'
-import { Typography } from '@mui/material';
-
 import { AppContext } from "../../context/AppContext";
 
-export default function ButtonLabel(props: TypographyProps) {
-  const { text, textColor, align } = props
-  const { colorTheme } = useContext(AppContext)
-  
-  const color = textColor ? textColor : (colorTheme === "dark") ? "white" : "black"
+import { Box, Typography } from '@mui/material';
 
+// Font Awesome
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { TypographyProps } from '../../@types/TypographyProps';
+
+interface ButtonLabelProps extends TypographyProps {
+  iconSize?: string;
+  startIcon?: IconDefinition;
+  endIcon?: IconDefinition;
+}
+
+export default function ButtonLabel(props: ButtonLabelProps) {
+  const { 
+    text, 
+    startIcon,
+    endIcon,
+    align = 'center' 
+  } = props
+
+  const { colorTheme } = useContext(AppContext)
+
+  const textColor = (colorTheme === "dark") ? "white" : "black"
+  const iconSize = 'lg'
+
+  
   return (
-    <Typography 
-      noWrap
-      children={text}
-      color={color}
-      align={align} 
-      sx={{
-        fontWeight: 700,
-        fontSize: '1.2rem',
-        lineHeight: '1.8rem',
-        '@media (min-width: 600px)': {
-          fontSize: '1.7rem',
-        lineHeight: '2.1rem',
-        },
-        '@media (min-width: 900px)': {
-          fontSize: '1.5rem',
-        lineHeight: '1.8rem',
-        },
-      }}
-    />
+    <>
+      {
+        startIcon ? (
+          <Box paddingX={2} >
+            <FontAwesomeIcon icon={startIcon} color={textColor} size={iconSize} />
+          </Box>
+        ) : (
+          <></>
+        )
+      }
+      <Typography 
+        noWrap
+        children={text}
+        color={textColor}
+        align={align} 
+        sx={{
+          fontWeight: 700,
+          fontSize: '1.2rem',
+          lineHeight: '1.8rem',
+          '@media (min-width: 600px)': {
+            fontSize: '1.7rem',
+          lineHeight: '2.1rem',
+          },
+          '@media (min-width: 900px)': {
+            fontSize: '1.5rem',
+          lineHeight: '1.8rem',
+          },
+        }}
+      />
+      {
+        endIcon ? (
+          <Box paddingX={2} >
+            <FontAwesomeIcon icon={endIcon} color={textColor} size={iconSize} />
+          </Box>
+        ) : (
+          <></>
+        )
+      }
+    </>
+
+
+
+    
   )
 }
