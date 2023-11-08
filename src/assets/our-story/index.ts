dd5db7019cbccb2d1f6937f0c0f003e34c1025f6
Nigel Wilson <nigel@stemgarden.org>
2023-11-07 18:26:12 -0600

    Create index.ts

---
+++ src/assets/our-story/index.ts
@@ -0,0 +1,4 @@
+export {default as trash_in_dirt} from './trash_in_dirt.jpg';
+export {default as toolshed} from './toolshed.jpg'
+export {default as tearoom} from './tearoom.jpg'
+export {default as solar_panels_on_classroom} from './solar_panels_on_classroom.jpg'
