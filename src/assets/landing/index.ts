41c5b0ae98ee93eb2f5ac28efb8e5873d03da479
Nigel Wilson <nigel@stemgarden.org>
2023-11-07 18:24:40 -0600

    Create image export index files

--- src/assets/landing/index.ts
+++ src/assets/landing/index.ts
@@ -2,9 +2,5 @@ export {default as leafy_background} from './leafy_background.jpg';
 export {default as practicing_math_facts} from './practicing_math_facts.jpg';
 export {default as girls_playing_connect_four} from './girls_playing_math_fact_connect_four.jpg';
 export {default as banana_papaya_turmeric_flower} from './banana_papaya_turmeric.jpg';
-export {default as trash_in_dirt} from './trash_in_dirt.jpg';
-export {default as toolshed} from './toolshed.jpg'
-export {default as tearoom} from './tearoom.jpg'
-export {default as solar_panels_on_classroom} from './solar_panels_on_classroom.jpg'
 
 
