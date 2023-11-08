9c02127fa54a9c563c47e887b759bf8e67612424
Nigel Wilson <nigel@stemgarden.org>
2023-11-07 18:24:02 -0600

    Switch back to enum from type

--- src/@types/TypographyProps.d.ts
+++ src/@types/TypographyProps.d.ts
@@ -1,7 +1,7 @@
-// enum TextColorEnum {
-//     black = 'black',
-//     white = 'white'
-// }
+export enum TextColorEnum {
+    black = 'black',
+    white = 'white'
+}
 
 // interface TypographyProps {
 //     text: string;
@@ -9,12 +9,12 @@
 //     gutterBottom: boolean;
 // }
 
-type TextColorEnum = 'black' | 'white'
+// type TextColorEnum = 'black' | 'white'
 
 type AlignEnum = 'center' | 'left'
 
 
-type TypographyProps = {
+interface TypographyProps {
     text: string;
     textColor?: TextColorEnum;
     align?: AlignEnum;
