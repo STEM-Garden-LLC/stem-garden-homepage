aeae2215e8f544ef70b9be8b8ba225097eb4cdb2
Nigel Wilson <nigel@stemgarden.org>
2023-11-16 10:06:38 -0600

    Move and update landing page text file

---
+++ src/pages/landing-page/OurStorySection.tsx
@@ -0,0 +1,6 @@
+          <Paragraph text={landing_page_text.our_story_summary_1} />
+          <PictureRow imgUrls={[trash_in_dirt, toolshed]} />
+          <Paragraph text={landing_page_text.our_story_summary_2} />
+          <Paragraph text={landing_page_text.our_story_summary_3} />
+          <PictureRow imgUrls={[tearoom, solar_panels_on_classroom]} />
+          <Paragraph text={landing_page_text.our_story_summary_4} />
