df6c5f2b0fd823bc9fbca6faa72675824a2eda7a
Nigel Wilson <nigel@stemgarden.org>
2023-11-22 20:58:44 -0600

    Edit all other images

---
+++ src/assets/what-we-grow/index.ts
@@ -0,0 +1,10 @@
+// OTHER
+
+export {default as limes} from './limes.jpg'
+export {default as carrots} from './carrots.jpg'
+export {default as radishes} from './radishes.jpg'
+export {default as tobacco} from './tobacco_plant.jpg'
+export {default as guavas_on_tree} from './guavas_on_tree.jpg'
+export {default as colorful_harvest} from './colorful_harvest.jpg'
+
+export {default as banana_types} from './banana_types.png'
--- src/assets/what-we-grow/other/index.ts
+++
@@ -1,9 +0,0 @@
-// OTHER
-
-export {default as limes} from './limes.jpg'
-export {default as carrots} from './carrots.jpg'
-export {default as radishes} from './radishes.jpg'
-export {default as tobacco} from './tobacco_plant.jpg'
-export {default as guavas_on_tree} from './guavas_on_tree.jpg'
-export {default as colorful_harvest} from './colorful_harvest.jpg'
-
