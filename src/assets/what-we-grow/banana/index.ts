8ddbbb317e06196384299092eadf05d1dec8c4ec
Nigel Wilson <nigel@stemgarden.org>
2023-11-22 20:54:27 -0600

    Edit banana images

--- src/assets/what-we-grow/banana/index.ts
+++ src/assets/what-we-grow/banana/index.ts
@@ -1,6 +1,9 @@
 // BANANA
 
-export {default as banana_types} from './banana_types.png'
-export {default as banana_bunch} from './banana_bunch.jpg'
+export {default as dwarf_cavendish_bunch} from './dwarf_cavendish_bunch.jpg'
+
+export {default as holding_burrows} from './holding_burrows.jpg'
 export {default as banana_flower} from './banana_flower.jpg'
 export {default as banana_hands} from './banana_hands.jpg'
+export {default as banana_green_bunch} from './banana_green_bunch.jpg'
+
