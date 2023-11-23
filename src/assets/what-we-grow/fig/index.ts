1b89e16c7b993ac07cf037fcbfe8ba82e76e703d
Nigel Wilson <nigel@stemgarden.org>
2023-11-22 20:56:21 -0600

    Edit figs images

--- src/assets/what-we-grow/fig/index.ts
+++ src/assets/what-we-grow/fig/index.ts
@@ -1,9 +1,14 @@
 // FIG
 
+export {default as fig_harvest} from './fig_harvest.jpg'
+
+export {default as figs_in_colander} from './figs_in_colander.jpg'
 export {default as figs_on_tree} from './figs_on_tree.jpg'
+export {default as figs_on_tree_green} from './figs_on_tree_green.jpg'
 export {default as fig_missing_bite} from './fig_missing_bite.jpg'
 export {default as fig_tree} from './fig_tree.jpg'
-export {default as fig_harvest} from './fig_harvest.jpg'
-export {default as three_figs} from './three_figs.jpg'
 export {default as buckets_of_figs} from './buckets_of_figs.jpg'
-export {default as bags_of_figs} from './bags_of_figs.jpg'
+export {default as figs_in_freezer_bags} from './figs_in_freezer_bags.jpg'
+export {default as figs_sliced_for_dehydrator} from './figs_sliced_for_dehydrator.jpg'
+
+
