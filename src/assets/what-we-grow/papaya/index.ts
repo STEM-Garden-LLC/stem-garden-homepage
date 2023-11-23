9555690166d484a026e5e9b69a1a39a6ec03df34
Nigel Wilson <nigel@stemgarden.org>
2023-11-22 20:58:00 -0600

    Edit papaya images

--- src/assets/what-we-grow/papaya/index.ts
+++ src/assets/what-we-grow/papaya/index.ts
@@ -1,7 +1,11 @@
 // PAPAYA
 
-export {default as papaya_halved} from './papaya/papaya_halved.jpg'
-export {default as papaya_fruit} from './papaya/papaya_fruit.jpg'
-export {default as papaya_picking} from './papaya/papaya_picking.jpg'
-export {default as papaya_leaf} from './papaya/papaya_leaf.jpg'
-export {default as papaya_tree_with_green_fruit} from './papaya/papaya_tree_with_green_fruit.jpg'
+export {default as papaya_halved} from './papaya_halved.jpg'
+
+export {default as papaya_pile} from './papaya_pile.jpg'
+// export {default as papaya_picking} from './papaya_picking.jpg'
+export {default as papaya_leaf} from './papaya_leaf.jpg'
+export {default as papaya_tree_with_green_fruit} from './papaya_tree_with_green_fruit.jpg'
+export {default as papaya_tree_with_ripe_fruit} from './papaya_tree_with_ripe_fruit.jpg'
+export {default as guatemalan_neighbors_get_papaya} from './guatemalan_neighbors_get_papaya.jpg'
+export {default as papaya_in_freezer_bags} from './papaya_in_freezer_bags.jpg'
