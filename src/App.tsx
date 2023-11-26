0769bbb0e5c35f4b9080f6241d3460d8c5c2ed45
Nigel Wilson <nigel@stemgarden.org>
2023-11-26 09:24:40 -0600

    Clean up unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState, useContext } from 'react'
 import { AppContextProvider } from "./context/AppContext";
 
 import { BrowserRouter } from "react-router-dom"
-
 
 // COMPONENTS
 import Navbar from "./components/navigation/Navbar"
 import MainRouter from './MainRouter'
 
-
 // MUI
-import theme from "./theme"
-import { ThemeProvider } from '@mui/material/styles';
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import stemGardenLogoWithBG from '/sg-logo-192px.png'
-import stemGardenLogo from '/sg-logo-transparent-bg.png'
-import './App.css'
+import theme from "../src/theme"
+import { ThemeProvider } from '@mui/material';
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <> 
       <AppContextProvider >
         <ThemeProvider theme={theme}>
           <BrowserRouter>
+
             <Navbar />
+
             <MainRouter />
-          </BrowserRouter>
+
+
+            </BrowserRouter>
         </ThemeProvider>
       </AppContextProvider>
     </>
