75f425f6641578afd8ad821342589cd74f36aa02
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 14:33:48 -0600

    Add FontWeight to TypographyProps

--- src/@types/TypographyProps.ts
+++ src/@types/TypographyProps.ts
@@ -13,10 +13,17 @@ export enum TextColorEnum {
 
 type AlignEnum = 'center' | 'left' | 'right'
 
+export enum FontWeightEnum {
+    light = '300',
+    normal = '400',
+    bold = '500',
+    extraBold = '700'
+}
 
 export interface TypographyProps {
     text: string;
     textColor?: TextColorEnum;
     align?: AlignEnum;
+    fontWeight?: FontWeightEnum;
     gutterBottom?: boolean;
 }
