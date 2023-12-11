10960c6d5f1dd82389c90d71bebe424d54895bcc
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 16:00:02 -0600

    Create ChessPage.tsx

---
+++ src/pages/services/ChessPage.tsx
@@ -0,0 +1,15 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+export default function ChessPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Chess" />
+      <Subtitle text="test" />
+    </ThemedBackground>
+  )
+}
