501a55e90da90ac865e82521826d31133e94fd43
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 14:47:21 -0600

    Consolidate font size declarations
    Make file more readable by setting all font sizes in one object and line heights in another rather than declaring in line in a series of @media queries.

--- src/components/typography/Subtitle.tsx
+++ src/components/typography/Subtitle.tsx
@@ -15,6 +15,20 @@ export default function Subtitle(props: TypographyProps) {
   const padding = (gutterBottom) ? "0 0 0.5rem" : "0"
   const color = textColor ? textColor : (colorTheme === "dark") ? "white" : "black"
 
+  const fontSizes = {
+    xs: '1.4rem',
+    sm: '1.6rem',
+    md: '1.9rem',
+    lg: '2.2rem'
+  }
+
+  const lineHeights = {
+    xs: '1.8rem',
+    sm: '2.0rem',
+    md: '2.3rem',
+    lg: '2.8rem'
+  }
+
   return (
     <Typography 
       children={text}
@@ -22,21 +36,20 @@ export default function Subtitle(props: TypographyProps) {
       p={padding}
       sx={{
         align: align,
-        // textShadow: '0.12rem 0.12rem 0.2rem black',
         fontWeight: 400,
-        fontSize: '1.4rem',
-        lineHeight: '2.2rem',
+        fontSize: fontSizes.xs,
+        lineHeight: lineHeights.xs,
         '@media (min-width: 450px)': {
-          fontSize: '1.6rem',
-          lineHeight: '2.0rem',
+          fontSize: fontSizes.sm,
+          lineHeight: lineHeights.sm,
         },
         '@media (min-width: 600px)': {
-          fontSize: '1.9rem',
-          lineHeight: '2.3rem',
+          fontSize: fontSizes.md,
+          lineHeight: lineHeights.md,
         },
         '@media (min-width: 900px)': {
-          fontSize: '2.2rem',
-          lineHeight: '2.8rem', 
+          fontSize: fontSizes.lg,
+          lineHeight: lineHeights.lg, 
         },
       }}
     />
