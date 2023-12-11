201703d226611503d7f53ef8b3669323d3d00ce3
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 15:59:58 -0600

    Create DivisibilityPlayground.tsx

---
+++ src/pages/resources/DivisibilityPlayground.tsx
@@ -0,0 +1,14 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title } from '../../components/typography'
+
+
+export default function DivisibilityPlaygroundPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Divisibility playground" />
+    </ThemedBackground>
+  )
+}
