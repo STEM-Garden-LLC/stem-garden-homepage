a9393a03dc6479f32b0621995810b897adef53ad
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 14:35:24 -0600

    Update index.ts

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,4 +1,6 @@
-export {default as Navbar} from './Navbar';
+export {default as Navbar} from './ResponsiveNavbar';
+// No need to export Mobile and Desktop versions separately
+
 export {default as Footer} from './Footer';
 export {default as ButtonWithIcon} from './ButtonWithIcon';
 export {default as ScrollToTopButton} from './ScrollToTopButton';
