befe6b3428c2787cc0b3bfa4ddf311ba97fcb296
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 16:00:15 -0600

    Create TutoringPage.tsx

---
+++ src/pages/services/TutoringPage.tsx
@@ -0,0 +1,15 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+export default function TutoringPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Tutoring Services" />
+      <Subtitle text="test" />
+    </ThemedBackground>
+  )
+}
