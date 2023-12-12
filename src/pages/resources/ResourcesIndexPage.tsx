3cea6dd0e2ac5c934b9d6192bafb716c5189c7c9
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 18:08:00 -0600

    Create ResourcesIndexPage.tsx

---
+++ src/pages/resources/ResourcesIndexPage.tsx
@@ -0,0 +1,15 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+export default function ResourcesIndexPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Resources" />
+      <Subtitle text="test" />
+    </ThemedBackground>
+  )
+}
