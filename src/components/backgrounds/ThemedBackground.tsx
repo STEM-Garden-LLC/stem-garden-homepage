0a4237434865b2d4fd82949d4aa28a89b292cb3a
Nigel Wilson <nigel@stemgarden.org>
2023-12-12 14:24:20 -0600

    Make ThemedBackground component work
    This was previously happening due to some CSS rules from the starter template and the component name was suggesting it did something it did not do.

--- src/components/backgrounds/ThemedBackground.tsx
+++ src/components/backgrounds/ThemedBackground.tsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react'
+import { AppContext } from '../../context/AppContext';
+
 // MUI
 import { Box, Container } from '@mui/material';
 
 export default function ThemedBackground(props: any) {
   const { children } = props
+  const { colorTheme } = useContext(AppContext)
+
+  const bgColor = colorTheme === 'dark' ? '#222' : '#FFF'
   
   return (
-    <Box sx={{ width: '100vw' }} >
+    <Box sx={{ width: '100vw', backgroundColor: bgColor }} >
       <Container
         maxWidth='md' 
         sx={{
