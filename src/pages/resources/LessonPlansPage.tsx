6db26b73afadda9ab4f3235720214ec5a11644de
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 18:07:50 -0600

    Create LessonPlansPage.tsx

---
+++ src/pages/resources/LessonPlansPage.tsx
@@ -0,0 +1,15 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+export default function LessonPlansPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Lesson Plans" />
+      <Subtitle text="test" />
+    </ThemedBackground>
+  )
+}
