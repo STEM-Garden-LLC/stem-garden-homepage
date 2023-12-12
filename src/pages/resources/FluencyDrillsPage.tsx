4ce1837fd8136963a69125900c80080ef062e408
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 18:07:48 -0600

    Create FluencyDrillsPage.tsx

---
+++ src/pages/resources/FluencyDrillsPage.tsx
@@ -0,0 +1,15 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+export default function FluencyDrillsPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Fluency Drills" />
+      <Subtitle text="test" />
+    </ThemedBackground>
+  )
+}
