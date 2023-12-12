ff18cfb018beedb6566bad18efe4f303df308aef
Nigel Wilson <nigel@stemgarden.org>
2023-12-11 18:07:54 -0600

    Create PuzzlesPage.tsx

---
+++ src/pages/resources/PuzzlesPage.tsx
@@ -0,0 +1,15 @@
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+export default function PuzzlesPage() {
+  
+  return (
+    <ThemedBackground>
+      <Title text="Puzzles" />
+      <Subtitle text="test" />
+    </ThemedBackground>
+  )
+}
