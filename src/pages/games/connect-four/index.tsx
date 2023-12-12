1b79d1c28a84984acf27050ab80e7a7151517a7b
Nigel Wilson <nigel@stemgarden.org>
2023-12-12 10:38:52 -0600

    Create connect four index file

---
+++ src/pages/games/connect-four/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// import { Container, Grid } from '@mui/material';
+
+import { ThemedBackground } from "../../components/backgrounds";
+import { Title, Subtitle } from '../../components/typography'
+
+
+
+// import { ConnectFourContextProvider } from "./ConnectFourContext";
+// import ConnectFourController from './ConnectFourController';
+
+export default function ConnectFourRoot() {
+  return (
+    // <ConnectFourContextProvider >
+    //   <ConnectFourController />
+    // </ConnectFourContextProvider>
+    <ThemedBackground>
+      <Title text="Connect Four Root" />
+    </ThemedBackground>
+  )
+}
