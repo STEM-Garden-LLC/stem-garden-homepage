200045c04992969cb22ce484f48b299cf60eb17c
Nigel Wilson <nigel@stemgarden.org>
2023-12-12 17:41:45 -0600

    Create index of tutoring pics

---
+++ src/assets/tutoring/index.ts
@@ -0,0 +1,5 @@
+export {default as bike_gear_ratios_tutoring} from './bike_gear_ratios_tutoring.jpg';
+export {default as ct_balance_scale} from './ct_balance_scale.jpg'
+export {default as guitar_fret_measuring} from './guitar_fret_measuring.jpg'
+export {default as jakob_at_graduation} from './jakob_at_graduation.jpg'
+export {default as multiple_choice_test} from './multiple_choice_test.jpg'
