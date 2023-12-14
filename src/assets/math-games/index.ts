893a01ab4447276ade8ae7c2eb763eb68db4d9b0
Nigel Wilson <nigel@stemgarden.org>
2023-12-14 14:00:05 -0600

    Create math games asses index

---
+++ src/assets/math-games/index.ts
@@ -0,0 +1,6 @@
+export {default as connect_four_screenshot} from './connect_four_screenshot.jpg';
+export {default as fifteen_game_screenshot} from './fifteen_game_screenshot.png';
+export {default as tic_tac_toe_screenshot} from './tic_tac_toe_coach_screenshot.png';
+export {default as under_construction} from './under_construction.png';
+
+
