0540dfcd5ea8ec0c8c77837cc2a33de8513c1784
Nigel Wilson <nigel@stemgarden.org>
2023-12-13 19:06:58 -0600

    Adjust padding

--- src/pages/resources/ResourcesIndexPage.tsx
+++ src/pages/resources/ResourcesIndexPage.tsx
@@ -8,7 +8,7 @@ export default function ResourcesIndexPage() {
   
   return (
     <ThemedBackground>
-      <Title text="Resources" />
+      <Title text="Resources" gutterBottom />
       <Subtitle text="test" />
     </ThemedBackground>
   )
