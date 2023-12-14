00fb8eb6bf5ea3431a797bacb165aa1534c7b0a3
Nigel Wilson <nigel@stemgarden.org>
2023-12-13 19:05:06 -0600

    Convert hardcoded strings to ColorThemeEnum values

--- src/hooks/useColorTheme.ts
+++ src/hooks/useColorTheme.ts
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 
+import { ColorThemeEnum } from '../@types/ColorTheme'
+
 // May want to change these hooks to use the Client Height property of the root <html> element.  
 // I think that the window.innerHeight currently used is counting height I don't actually want. 
 // document.documentElement.clientWidth & document.documentElement.clientHeight
@@ -8,15 +10,15 @@ export default function useColorTheme() {
         "(prefers-color-scheme: dark)"
     );
 
-    const [colorTheme, setColorTheme] = useState(mediaQueryPrefersDark.matches ? 'dark' : 'light')
+    const [colorTheme, setColorTheme] = useState(mediaQueryPrefersDark.matches ? ColorThemeEnum.dark : ColorThemeEnum.light)
 
     const updateColorTheme = (event: any) => {
-        event.matches ? setColorTheme('dark') : setColorTheme('light');
+        event.matches ? setColorTheme(ColorThemeEnum.dark) : setColorTheme(ColorThemeEnum.light);
     }
 
     useEffect(() => {
         if (mediaQueryPrefersDark.matches) {
-            setColorTheme('dark');
+            setColorTheme(ColorThemeEnum.dark);
         }
 
         // This callback will fire if the preferred color scheme changes without a reload
