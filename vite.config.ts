194430fa06813a817d037e8ec10460b880457b26
Nigel Wilson <nigel@stemgarden.org>
2023-12-13 19:01:52 -0600

    Add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@typography': path.resolve(__dirname, './src/components/typography'),
+    },
+  },
   plugins: [react()],
 })
