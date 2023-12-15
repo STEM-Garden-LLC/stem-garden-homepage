6c779bd00dae3e1d7e52d033602d478a59343c0e
Nigel Wilson <nigel@stemgarden.org>
2023-12-15 14:13:50 -0600

    Draft PuzzleCardProps type

--- src/@types/Cards.ts
+++ src/@types/Cards.ts
@@ -2,6 +2,8 @@
 import { TextColorEnum } from "./TypographyProps";
 import { LinkTypeEnum } from './Links'
 
+import { PuzzleTag } from "@data/puzzlesData";
+
 export interface PictureCardProps {
   title: string;
   imageUrl: string;
@@ -12,5 +14,20 @@ export interface PictureCardProps {
   bgColor?: string;
   cardWidth: string;
   cardHeight: string;
+}
+
+
+export interface PuzzleCardProps {
+  name: string;
+  tags: PuzzleTag[];
+  imageUrl: string;
+  googleDriveFileId: string;
+  source?: string;
+  // linkType?: LinkTypeEnum;
+  // onClick?: Function;
+  // textColor?: TextColorEnum;
+  // bgColor?: string;
+  // cardWidth: string;
+  // cardHeight: string;
 }
 
