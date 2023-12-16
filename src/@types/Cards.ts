da7af77fec03c4aa6a9491cf99cf5926b50ef7ba
Nigel Wilson <nigel@stemgarden.org>
2023-12-16 16:57:22 -0600

    Add Props type for Game Card

--- src/@types/Cards.ts
+++ src/@types/Cards.ts
@@ -25,11 +25,12 @@ export interface PuzzleCardProps {
   imageUrl: string;
   googleDriveFileId: string;
   source?: string;
-  // linkType?: LinkTypeEnum;
-  // onClick?: Function;
-  // textColor?: TextColorEnum;
-  // bgColor?: string;
-  // cardWidth: string;
-  // cardHeight: string;
 }
 
+export interface GameCardProps {
+  title: string;
+  imageUrl: string;
+  linkTo: string;
+  description: string;
+  disabled?: boolean;
+}
