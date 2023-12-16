7720eb77c08f756c6cdbcd71ff6c0dfb8c26524f
Nigel Wilson <nigel@stemgarden.org>
2023-12-16 17:21:22 -0600

    Update gamesData.ts

--- src/data/gamesData.ts
+++ src/data/gamesData.ts
@@ -1,9 +1,4 @@
-import { PictureCardProps } from '../@types/Cards'
-import { ColorsEnum } from '../@types/Colors'
-import { TextColorEnum } from "../@types/TypographyProps";
-import { LinkTypeEnum } from '../@types/Links'
-
-import { navData } from './navigationData';
+import { GameCardProps } from '../@types/Cards'
 
 // IMAGES
 import { 
@@ -14,27 +9,49 @@ import {
 } from '@assets/math-games'
 // } from '@assets/math-games' // Not Working
 
-const connect_four_description = "The classic game with a twist. Each time you select a column to drop a chip in you will be presented with a math question. Answer it correctly or your turn will be skipped!"
-const tic_tac_toe_description = "Play the most underestimated strategy game of all time with a coach that will help you see its true depth. Tic Tac Toe serves as a wonderful tool for introducing kids to mathematical proof, tree diagrams, symmetry, and counting techniqes. "
-const fifteen_game_description = "Two players go head to head trying to collect a set of three cards that add up to 15. This game is a fantastic tool for building mental addition skills. For advanced players it has connections to combinations, permutations, and magic squares."
 
-
-
-
-const gamesData = [
+const gamesData : GameCardProps[] = [
   {
-    title: 'The 15 Game',
+    title: "The Fifteen Game",
+    linkTo: "/games/the-15-game",
     imageUrl: fifteen_game_screenshot,
-    linkTo: '/games/fifteen-game',
-    // linkType: LinkTypeEnum;
-    // onClick?: Function;
-    // textColor?: TextColorEnum.white
+    disabled: false,
+    description: ["Two players go head to head trying to collect a set of three cards that add up to 15. This game is a fantastic tool for building mental addition skills. For advanced players it has connections to combinations, permutations, and magic squares."]
   },
   {
-    title: 'The 15 Game',
-    imageUrl: 'test',
-    linkTo: '/games/the-15-game/*',
-  }
+    title: "Math Fact Connect Four",
+    linkTo: "/games/connect-four",
+    imageUrl: connect_four_screenshot,
+    disabled: false,
+    description: ["The classic game with a twist: Each time you select a column to drop a chip in you will be presented with a math question. Answer it correctly or your turn will be skipped!"],
+  },
+  {
+    title: "Tic Tac Toe Coach",
+    linkTo: "/games/tic-tac-toe",
+    imageUrl: tic_tac_toe_screenshot,
+    disabled: false,
+    description: [
+      "The game of Tic Tac Toe presents fantastic but often overlooked opportunities for introducing students to mathematical ideas including tree diagrams, symmetry, and rigorous proof. It is both popular and simple, accessible to very young kids while still having enough depth to be interesting to adults.",
+      "This is your chance to play against a master coach who will pose questions leading you to insight in any position. If you get stumped or want to check your answer the coach will show you which of the possible moves lead to wins, losses, and draws."
+    ]
+  },
+  {
+    title: "Classic Nim",
+    linkTo: "the-15-game",
+    imageUrl: under_construction,
+    disabled: true,
+    description: [
+      "In Nim, players take turns removing stones from one of a few piles until one is forced to take the last stone, losing the game.",
+      "There is a very interesting trick for mastering this game quickly. It is based on binary numbers and is commonly used with an advanced form of finger counting."
+    ]
+  },
+  // {
+  //   title: "The Thirty Four Game",
+  //   linkTo: "the-34-game",
+  //   imageUrl: fifteen_game_screenshot,
+  //   disabled: false,
+  //   description: ["Two players go head to head trying to collect a set of four cards that add up to 34. You can adjust the maximum number of cards each player can have at once and whether they choose the card to discard or use a FIFO discard policy."]
+  // },
 ]
 
 export default gamesData;
