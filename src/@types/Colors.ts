dca5e04f4b8c1f33dba72efc27b96f4de14977af
Nigel Wilson <nigel@stemgarden.org>
2023-12-17 22:37:35 -0600

    Create enum of colors specific to fifteen game

--- src/@types/Colors.ts
+++ src/@types/Colors.ts
@@ -5,4 +5,13 @@ export enum ColorsEnum {
   lightGrey = '#333333',
   offWhite = '#F0F0E6',   // '232,232,224'
   white = '#FFFFFF',
+
 }
+
+export enum FifteenGameColorsEnum {
+  playerOne = '#ffea00',    // yellow
+  playerTwo = '#1020ff',    // blue
+  highlightWin = '#009900', // bright green
+
+}
+
