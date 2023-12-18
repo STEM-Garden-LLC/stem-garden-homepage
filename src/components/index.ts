90b7136d64ff3780916fd49fe47ce36940c5b972
Nigel Wilson <nigel@stemgarden.org>
2023-12-18 09:24:58 -0600

    Create components index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,13 @@ export {default as Navbar} from './navigation/ResponsiveNavbar';
 export {default as Footer} from './navigation/Footer';
 export {default as ButtonWithIcon} from './buttons/ButtonWithIcon';
 export {default as ScrollToTopButton} from './buttons/ScrollToTopButton';
+
+
+// TYPOGRAPHY
+
+export {default as Paragraph} from './typography/Paragraph';
+export {default as Title} from './typography/Title';
+export {default as Subtitle} from './typography/Subtitle';
+export {default as Heading} from './typography/Heading';
+export {default as ButtonLabel} from './typography/ButtonLabel';
+export {default as MenuItem} from './typography/MenuItem';
