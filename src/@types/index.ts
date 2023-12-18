f3d9b521acc90b808a01c80c988a010e6b726e27
Nigel Wilson <nigel@stemgarden.org>
2023-12-18 09:24:02 -0600

    Create @types index

---
+++ src/@types/index.ts
@@ -0,0 +1,8 @@
+
+export type { PictureCardProps, PuzzleCardProps, GameCardProps } from './Cards';
+export type { ColorThemeEnum, ColorThemeContextType } from './ColorTheme'
+export type { LinkTypeEnum } from './Links'
+export type { AlignEnum, FontWeightEnum, TypographyProps } from './TypographyProps'
+
+
+
