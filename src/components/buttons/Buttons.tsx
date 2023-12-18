18ec5b18861414c7d8af465a7c0d0ea7becdee91
Nigel Wilson <nigel@stemgarden.org>
2023-12-18 08:39:13 -0600

    Create folder for Button components

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -4,26 +4,48 @@ import { Link as RouterLink } from 'react-router-dom';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faRotateLeft, faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faRotateLeft, faAnglesLeft, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
-import { gameOver } from "../helpers/magicSquareHelpers";
+import { gameOver } from "../../pages/games/helpers/magicSquareHelpers";
 
 
 type GameButtonProps = {
   label: string,
+  icon: IconDefinition,
   onClick: Function,
-  active?: boolean
+  disabled?: boolean,
+  selected?: boolean,
+}
+
+export function GameButton(props: GameButtonProps) {
+  const { label, icon, onClick, disabled = false, selected = false } = props
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={() => onClick()}
+      disabled={disabled}
+      sx={{ flexGrow: 2, mx: 1 }}
+    >
+      <Box display="flex" alignContent="center" >
+        <FontAwesomeIcon icon={faHouse} size='lg' />
+      </Box>
+      <Typography children={label} variant="button" ml={1} display={{ xs: 'none', sm: 'block' }}  />
+      {/* <Typography children="Let" variant="button" display={{ xs: 'none', sm: 'block' }}  />
+      <Typography children="Bot Go First" variant="button" noWrap /> */}
+    </Button>
+  )
 }
 
 
-export function BotGoFirstButton(props) {
-  const { moveList, letBotGoFirst } = props
+export function BotGoFirstButton(props: GameButtonProps) {
+  const { movelist, letBotGoFirst } = props
   return (
     <Button
       variant="contained"
       color="primary"
       onClick={() => letBotGoFirst()}
-      disabled={moveList.length !== 0}
+      disabled={movelist.length !== 0}
       sx={{ flexGrow: 2, mx: 1 }}
     >
       <Typography children="Let" variant="button" display={{ xs: 'none', sm: 'block' }}  />
@@ -32,15 +54,14 @@ export function BotGoFirstButton(props) {
   )
 }
 
-export function DifficultyModeButton(props) {
-  const { thisButtonsMode, difficultyMode, changeDifficultyMode } = props
-  const selected = (thisButtonsMode === difficultyMode)
+export function DifficultyModeButton(props: GameButtonProps) {
+  const { label, selected, onClick: setDifficultyMode } = props
   return (
     <Button
-      children={thisButtonsMode}
+      children={label}
       sx={{flexGrow: 1, border: selected ? 'solid white 3px' : 'none', marginX: 2 }}
       variant={'contained'}
-      onClick={() => changeDifficultyMode(thisButtonsMode)}
+      onClick={() => setDifficultyMode(label)}
     />
   )
 }
@@ -63,18 +84,15 @@ export function HomeButton() {
 }
 
 
-export function NewGameButton(props) {
-  const { 
-    moveList, 
-    handleNewGameClick
-  } = props
+export function NewGameButton(props: GameButtonProps) {
+  const { disabled, onClick: handleNewGameClick } = props
 
   return (
     <Button
       onClick={() => handleNewGameClick()}
       variant="contained"
       color="primary"
-      disabled={!gameOver(moveList)}
+      disabled={!gameOver(movelist)}
       sx={{ flexGrow: 2 }}
     >
       <Box mr={1} display={{ xs: 'none', sm: 'flex' }} alignContent="center" >
@@ -86,9 +104,9 @@ export function NewGameButton(props) {
 }
 
 export function UndoMoveButton(props) {
-  const { moveList, handleUndoClick } = props
+  const { movelist, handleUndoClick } = props
 
-  const disabled = (moveList.length === 0 || gameOver(moveList))
+  const disabled = (movelist.length === 0 || gameOver(movelist))
   
   return (
     <Button
--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -2,5 +2,5 @@ export {default as Navbar} from './ResponsiveNavbar';
 // No need to export Mobile and Desktop versions separately
 
 export {default as Footer} from './Footer';
-export {default as ButtonWithIcon} from './ButtonWithIcon';
-export {default as ScrollToTopButton} from './ScrollToTopButton';
+export {default as ButtonWithIcon} from '../buttons/ButtonWithIcon';
+export {default as ScrollToTopButton} from '../buttons/ScrollToTopButton';
