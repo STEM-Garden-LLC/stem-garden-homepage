ab9cd0902d3936fb85611b8f263d7b0a1dd6a156
Nigel Wilson <nigel@stemgarden.org>
2023-12-18 09:24:17 -0600

    Formatting

--- src/@types/Theme.ts
+++ src/@types/Theme.ts
@@ -1,34 +1,33 @@
 declare module '@mui/material/styles' {
-    interface Theme {
-      palette: {
-        primary: {
-          main: string
-        },
-        common: {
-          white: string,
-          black: string,
-          transparent: string
-        }
+  interface Theme {
+    palette: {
+      primary: {
+        main: string
+      },
+      common: {
+        white: string,
+        black: string,
+        transparent: string
       }
-      status: {
-        danger: string;
-      };
     }
-    // allow configuration using `createTheme`
-    interface ThemeOptions {
-      palette: {
-        primary: {
-          main: string
-        },
-        common: {
-          white: string,
-          black: string,
-          transparent: string
-        }
-      }
-      status?: {
-        danger: string;
-      };
-    }
+    status: {
+      danger: string;
+    };
   }
-  
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    palette: {
+      primary: {
+        main: string
+      },
+      common: {
+        white: string,
+        black: string,
+        transparent: string
+      }
+    }
+    status?: {
+      danger: string;
+    };
+  }
+}
