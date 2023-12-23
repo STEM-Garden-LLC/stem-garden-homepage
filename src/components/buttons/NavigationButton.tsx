7dac32a6cfe799cff728d12cf3e27c070e074745
Nigel Wilson <nigel@stemgarden.org>
2023-12-23 17:30:39 -0600

    Create NavigationButton.tsx
    Same as old button but will replace it in phases

---
+++ src/components/buttons/NavigationButton.tsx
@@ -0,0 +1,54 @@
+// MUI
+import { Button } from '@mui/material';
+
+// Font Awesome
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+
+
+// CUSTOM COMPONENTS
+import { ButtonLabel } from '../typography';
+import { useContext } from 'react';
+import { ColorThemeContext } from '../../context/ColorThemeContext';
+
+type ButtonWithIconProps = {
+  label: string;
+  linkTo: string;
+  textColor?: string;
+  fontFamily?: string;
+  align?: string;
+  startIcon?: IconDefinition;
+  endIcon?: IconDefinition;
+  // iconSize?: string;
+}
+
+export default function ButtonWithIcon(props: ButtonWithIconProps) {
+  const { label, linkTo, startIcon, endIcon } = props
+  const { colorTheme } = useContext(ColorThemeContext)
+
+  const textColor = colorTheme === 'light' ? 'darkGrey' : 'white'
+
+  return (
+    <Button 
+      href={linkTo} 
+      variant="outlined"
+      sx={{ 
+        color: textColor, 
+        border: `solid ${textColor} 1px`, 
+        maxWidth: '400px',  
+        marginX: 'auto',  
+        marginY: 2,  
+        padding: 1,
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center'
+
+      }} 
+    >
+      <ButtonLabel 
+        text={label} 
+        startIcon={startIcon}
+        endIcon={endIcon} 
+      />
+    </Button>
+  )
+}
