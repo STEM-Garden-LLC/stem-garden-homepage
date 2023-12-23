ffa2d0c3d6318ce1e9a6378ca6781aa4301cc238
Nigel Wilson <nigel@stemgarden.org>
2023-12-23 17:31:14 -0600

    Replace ButtonWithIcon with NavigationButton

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export {default as Navbar} from './navigation/ResponsiveNavbar';
 // No need to export Mobile and Desktop versions separately
 
 export {default as Footer} from './navigation/Footer';
-export {default as ButtonWithIcon} from './buttons/ButtonWithIcon';
+export {default as ButtonWithIcon} from './buttons/NavigationButton';
 export {default as ScrollToTopButton} from './buttons/ScrollToTopButton';
 
 
@@ -14,3 +14,7 @@ export {default as Subtitle} from './typography/Subtitle';
 export {default as Heading} from './typography/Heading';
 export {default as ButtonLabel} from './typography/ButtonLabel';
 export {default as MenuItem} from './typography/MenuItem';
+
+// BUTTONS 
+export {default as NavigationButton} from './buttons/NavigationButton'
+export {default as GameButton} from './buttons/GameButton'
