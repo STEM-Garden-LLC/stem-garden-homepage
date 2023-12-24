8ea84ffe9ed70096761bf4110bd513fd13307c38
Nigel Wilson <nigel@stemgarden.org>
2023-12-23 21:01:55 -0600

    Comment out nonexistent page export

--- src/pages/games/fifteen-game/index.tsx
+++ src/pages/games/fifteen-game/index.tsx
@@ -1,4 +1,4 @@
-export { default as FifteenGamePage } from './FifteenGamePage'
+// export { default as FifteenGamePage } from './FifteenGamePage'
 export { default as Welcome } from './Welcome'
 export { default as PlayVsFriend } from './PlayVsFriend'
 export { default as PlayVsBot } from './PlayVsBot'
