b763cdf296e921cef34dfcea2d55e92c05bfb928
Nigel Wilson <nigel@stemgarden.org>
2023-12-23 20:59:13 -0600

    Extract fifteenGameData

---
+++ src/data/fifteenGameData.ts
@@ -0,0 +1,36 @@
+const fifteenGameData : any = {
+  description: {
+    heading: '',
+    paragraphs: [
+      'A turn-based two-player strategy game. Traditionally played with a set of cards numbered one through nine, players take turns claiming one card at a time.',  'The goal is to be first to collect a set of three cards that adds up to exactly fifteen. You may collect more than three cards, but only subsets of three count as winning sets. If all nine cards are claimed with neither player making a winning set the game ends a draw.',
+    ]
+  },
+  general: {
+    heading: 'General',
+    paragraphs: [
+      'This game truly stands out for having something to offer students at a very wide range of levels of math expertise. Each round only takes takes a few minutes to play, making it ideal for a brain-break activity during class. If you want to set up a bracket and have a class-wide or school-wide championship tournament, I recommend the free tool available at Bracket HQ.',
+    ]
+  },
+  elementarySchool: {
+    heading: 'Elementary: Mental Math and Thinking Out Loud',
+    paragraphs: [
+      'The Fifteen Game is a fun way to build automaticity at mentally computing sums and differences within 20. Even for students who have mastered basic addition, trying to formulate strategies that involve offense and defense will push them. With repeated play they start to recognize patterns that make the game easier to play.', 
+      'How many different ways could you win if you go first and take the 5? How many pairs are there that add up to 10? Find all the ways you could win if your opponent goes first and takes the 5. What do they have in common? Be sure to get kids talking! Thinking out loud is a valuable life skill that games like this are excellent tools for teaching. Keep playing against the bot in "Easy" mode until you win half of your games and never lose.',
+    ]
+  },
+  middleSchool: {
+    heading: 'Middle School: Combinations and Permutations',
+    paragraphs: [
+      'How many different three card combinations are there that sum to 15? How many different three card combinations are there total? What are the sums of all the other sets? Would this game be fun to play with a different target number than 15?',
+      'How many different orders are there that the players could claim the nine cards? Can you see why that question is actually quite difficult to answer? Can you come up with a simple approach that establishes a good estimate?'
+    ]
+  },
+  highSchool: {
+    heading: 'High School: Similarity Beneath the Surface & Rigorous Proof',
+    paragraphs: [
+      'One of the things that makes math such a powerful tool it\'s ability to draw our attention to those situations in life when our current problem can be solved by reusing a solution that to some other seemingly unrelated problem. ',
+    ]
+  },
+}
+
+export default fifteenGameData;
--- src/data/index.ts
+++ src/data/index.ts
@@ -3,5 +3,6 @@ export {default as navData} from './navigationData';
 export {default as gamesData} from './gamesData';
 export {default as puzzlesData} from './puzzlesData';
 export {default as whatWeGrowData} from './whatWeGrowData';
+export {default as fifteenGameData} from './fifteenGameData';
 
 
