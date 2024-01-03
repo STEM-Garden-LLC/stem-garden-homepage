b54829ccdf1e9462e62398a5a4743850ae2cf013
Nigel Wilson <nigel@stemgarden.org>
2024-01-02 21:47:39 -0600

    Split up export into two statements
    To get around export type error

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,6 +1,9 @@
 
 export type { PictureCardProps, PuzzleCardProps, GameCardProps } from './Cards';
-export type { ColorThemeEnum, ColorThemeContextType } from './ColorTheme'
+// export type { ColorThemeEnum, ColorThemeContextType } from './ColorTheme'
+export { ColorThemeEnum } from './ColorTheme'
+export type { ColorThemeContextType } from './ColorTheme'
+
 export type { LinkTypeEnum } from './Links'
 export type { AlignEnum, FontWeightEnum, TypographyProps } from './TypographyProps'
 
