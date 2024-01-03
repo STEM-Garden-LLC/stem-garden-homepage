bafd565915fdff547975e24618b5e8184d475cfc
Nigel Wilson <nigel@stemgarden.org>
2024-01-02 21:56:17 -0600

    Fix outcome rows layout

--- src/pages/games/fifteen-game/WinLossDrawDisplay.tsx
+++ src/pages/games/fifteen-game/WinLossDrawDisplay.tsx
@@ -1,98 +1,91 @@
 
 // COMPONENTS
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { Heading } from '@components/typography'
 
 // HELPERS
 import { playModeIsHumanVsHuman, playModeIsHumanVsBot } from '../helpers/magicSquareHelpers';
 
 // TYPES 
-import { FifteenGameColorsEnum, PlayModeEnum, PlayersEnum } from '../helpers/magicSquareTypes';
+import { FifteenGameColorsEnum, PlayModeEnum, PlayersEnum, OutcomesEnum } from '../helpers/magicSquareTypes';
+import { CenteredFlexBox } from '@/components';
 
 type WinLossDrawDisplayProps = {
   playMode: PlayModeEnum;
   winLossDrawRecord: number[];
-  humanVsHuman: boolean;
 }
 
 export default function WinLossDrawDisplay(props: WinLossDrawDisplayProps) {
   const { 
     playMode,
     winLossDrawRecord,
-    humanVsHuman
   } = props
 
   
   
   if (playModeIsHumanVsHuman(playMode)) {
     return (
-      <>
+      <CenteredFlexBox column >
         <OutcomeRow 
           label={PlayersEnum.playerOne}
           score={winLossDrawRecord[0]} 
-          humanVsHuman 
+          tileColor={FifteenGameColorsEnum.playerOne}
         />
         <OutcomeRow 
           label={PlayersEnum.playerTwo}
           score={winLossDrawRecord[1]} 
-          humanVsHuman 
+          tileColor={FifteenGameColorsEnum.playerTwo}
         />
         <OutcomeRow 
           label="Draw"
           score={winLossDrawRecord[2]} 
-          humanVsHuman 
+          tileColor={FifteenGameColorsEnum.transparent}
         />
-      </>
+      </CenteredFlexBox>
     )
   }
   else if (playModeIsHumanVsBot(playMode)) {
     return (
-      <>
+      <CenteredFlexBox column >
         <OutcomeRow 
           label="Human"
+          tileColor={FifteenGameColorsEnum.playerOne}
           score={winLossDrawRecord[0]} />
         <OutcomeRow 
           label="Bot"
+          tileColor={FifteenGameColorsEnum.playerTwo}
           score={winLossDrawRecord[1]} />
         <OutcomeRow 
           label="Draw"
+          tileColor={FifteenGameColorsEnum.transparent}
           score={winLossDrawRecord[2]} />
-      </>
+      </CenteredFlexBox>
     )
   }
 }
   
 type OutcomeRowProps =  {
-  label: PlayersEnum | string,
+  label: OutcomesEnum | string,
+  tileColor: FifteenGameColorsEnum,
   score: number, 
-  humanVsHuman?: boolean
 }
 
 function OutcomeRow(props: OutcomeRowProps) {
-  const { label, score } = props
-  
-  const tileColor = 
-    label === GameOutcomesEnum.playerOneWins ? 
-    FifteenGameColorsEnum.playerOne : 
-    outcome === GameOutcomesEnum.playerTwoWins ?
-    FifteenGameColorsEnum.playerTwo : 
-
-  
-  console.log(`Player: ${player}`)
-  console.log(`Label: ${label}`)
+  const { label, tileColor, score } = props
   
   return (
-    <Box display='flex' flexDirection='row' justifyContent='space-between' alignItems='center' >
-      <Box 
-        width='1.2rem'
-        height='1.2rem'
-        borderRadius='15%'
-        margin='0 .5rem 0 0'
-        backgroundColor={tileColor}
-      />
-      <Heading text={label} />
-      <Typography align='right' variant='h5' marginLeft='auto' children={score} />
-        
+    <Box display='flex' justifyContent='space-between' width='100%' >
+      <CenteredFlexBox paddingRight='3rem' >
+        <Box 
+          width='1.2rem'
+          height='1.2rem'
+          borderRadius='15%'
+          margin='0 .5rem 0 0'
+          bgcolor={tileColor}
+        />
+        <Heading text={`${label}: `} />
+      </CenteredFlexBox>
+      <Heading text={`${score}`} />
     </Box>
   )
 } 
