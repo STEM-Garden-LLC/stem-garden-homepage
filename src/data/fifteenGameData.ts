15f444986e504797df58cdfa2c68a7eb9e418d9d
Nigel Wilson <nigel@stemgarden.org>
2024-01-12 14:28:45 -0600

    Add teaching notes for High school

--- src/data/fifteenGameData.ts
+++ src/data/fifteenGameData.ts
@@ -1,4 +1,4 @@
-const fifteenGameData : any = {
+const fifteenGameData : Object = {
   description: {
     heading: '',
     paragraphs: [
@@ -22,13 +22,14 @@ const fifteenGameData : any = {
     heading: 'Middle School: Combinations and Permutations',
     paragraphs: [
       'How many different three card combinations are there that sum to 15? How many different three card combinations are there total? What are the sums of all the other sets? Would this game be fun to play with a different target number than 15?',
-      'How many different orders are there that the players could claim the nine cards? Can you see why that question is actually quite difficult to answer? Can you come up with a simple approach that establishes a good estimate?'
+      'How many different orders are there that the players could claim the nine cards? Can you see why that question is actually quite difficult to answer? Can you come up with a simple approach that establishes a good estimate, an upper or lower bound?'
     ]
   },
   highSchool: {
     heading: 'High School: Similarity Beneath the Surface & Rigorous Proof',
     paragraphs: [
-      'One of the things that makes math such a powerful tool it\'s ability to draw our attention to those situations in life when our current problem can be solved by reusing a solution that to some other seemingly unrelated problem. ',
+      'One of the things that makes math such a powerful tool it\'s ability to draw our attention to those situations in life when our current problem can be solved by reusing a solution that to some other seemingly unrelated problem. The logic behind Fifteen Game is actually identical to another game you already know. If you can figure out what other game it is the same as you will find that it makes the Fifteen game much easier to play well.',
+      'Play this game many times and you may notice that in Easy and Medium modes you can sometimes win but in Hard mode you never do, regardless of whether you go first or let the bot. Is that just because you haven\'t mastered the best strategy yet? Or is it because it\'s impossible? Try drawing a tree-diagram of all the possible move orders that shows that if neither player makes a "mistake" the game always ends in a draw. While you\'re at it you will need to come up with a very clear definition of the word mistake. You will save yourself a lot of work if you figure out what familiar game the Fifteen Game is identical to because doing so will allow you to ignore variations that are symmetrical. For instance, does it make any difference if you start the game by choosing 1 or 9 or are these two moves basically the same? What about 3 or 7?'
     ]
   },
 }
