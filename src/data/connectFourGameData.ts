0c12a867a109ab1b65b3548f988a0fcbb7ae540a
Nigel Wilson <nigel@stemgarden.org>
2024-01-19 16:40:05 -0600

    Create connectFourGameData.ts
    Add text to Connect Four welcome page

---
+++ src/data/connectFourGameData.ts
@@ -0,0 +1,29 @@
+import { TextSectionProps } from "@/@types";
+
+
+export const rulesSection: TextSectionProps = {
+  data: {
+    heading: 'Rules',
+    paragraphs: [
+      "Two players (or teams) take turns picking a column to drop a chip in. Chips always fall to the lowest available space in the selected column. The first player to get four consecutive chips in any row, column, or diagonal wins! But there's a catch: When you select a column a math question will pop up and if you answer incorrectly your turn will be skipped!"
+    ]
+  }
+}
+
+export const suggestedUseSection: TextSectionProps = {
+  data: {
+    heading: 'Suggested Use',
+    paragraphs: [
+      "Learning foundational math facts takes a lot of practice and there is no substitute for repetition. The goal of this game is to make doing all that repetitive practice a little more fun. All the questions in this game were designed to be answered either from memory or with mental calculation. It's ok to use your fingers if you have to, but don't use paper and pencil! Push yourself to try new mental math strategies like \"stepping stones\" and \"factor it first\"."
+    ]
+  }
+}
+
+export const thanksSection: TextSectionProps = {
+  data: {
+    heading: 'Thanks',
+    paragraphs: [
+      "I want to thank my students at College Track, the Waldorf School of New Orleans, and Bricolage Academy for inspiring me to create this game, helping test it, and suggesting ways to improve it. This game was originally played using laminated cards hung on the wall. It was my students' enthusiastic responses that made me want to convert it to a free web app so I could share it with other teachers and students."
+    ]
+  }
+}
