df59e1560e2abec25e6b63f887122fb65413c027
Nigel Wilson <nigel@stemgarden.org>
2024-01-19 16:40:39 -0600

    Update gamesData.ts
    Add more description to game cards

--- src/data/gamesData.ts
+++ src/data/gamesData.ts
@@ -16,14 +16,19 @@ const gamesData : GameCardProps[] = [
     linkTo: "/games/the-15-game",
     imageUrl: fifteen_game_screenshot,
     disabled: false,
-    description: ["Two players go head to head trying to collect a set of three cards that add up to 15. This game is a fantastic tool for building mental addition skills. For advanced players it has connections to combinations, permutations, and magic squares."]
+    description: [
+      "I first learned this game while teaching at a Waldorf School but it has ancient roots and something to offer students of all levels of mathematical experience.",
+      "In the Fifteen Game, two players go head to head trying to collect a set of three cards that add up to 15. It is a fantastic tool for building mental addition skills. For advanced players it has connections to combinations, permutations, and magic squares."]
   },
   {
     title: "Math Fact Connect Four",
     linkTo: "/games/connect-four",
     imageUrl: connect_four_screenshot,
     disabled: false,
-    description: ["The classic game with a twist: Each time you select a column to drop a chip in you will be presented with a math question. Answer it correctly or your turn will be skipped!"],
+    description: [
+      "The classic game of four-in-a-row with a twist: Each time you select a column to drop a chip in you will be presented with a math question. Answer it correctly or your turn will be skipped!",
+      "This game includes the option to select a type of arithmetic question from several different difficulty levels, making ideal to use again and again as students improve their skills."
+    ],
   },
   // {
   //   title: "Tic Tac Toe Coach",
@@ -41,8 +46,8 @@ const gamesData : GameCardProps[] = [
     imageUrl: under_construction,
     disabled: true,
     description: [
-      "In Nim, players take turns removing stones from one of a few piles until one is forced to take the last stone, losing the game.",
-      "There is a very interesting trick for mastering this game quickly. It is based on binary numbers and is commonly used with an advanced form of finger counting."
+      "In Nim, players take turns removing stones from one of a few piles until one player or the other is forced to take the last stone, losing the game when they do so.",
+      "There is a very interesting trick for mastering this game which is based on binary numbers. This trick is commonly used with an advanced form of finger counting. Most people think you can only use the fingers of one hand to count up to five but once you know this trick you'll see one hand can take you all the way to 31!"
     ]
   },
   // {
