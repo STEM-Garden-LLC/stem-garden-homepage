1b335d3446c02286cf300dedf6655402431ed2b0
Nigel Wilson <nigel@stemgarden.org>
2024-01-19 16:39:27 -0600

    Use extracted props type

--- src/components/typography/TextSection.tsx
+++ src/components/typography/TextSection.tsx
@@ -2,35 +2,24 @@
 
 // COMPONENTS
 import { Box } from '@mui/material';
-import { Heading, Subheading, Paragraph } from '@components/typography'
+import { Heading, Paragraph } from '@components/typography'
 import { AlignEnum } from '../../@types/TypographyProps';
 
-
-type TextSectionProps = {
-  data: {
-    heading: string,
-    paragraphs: string[]
-  }
-}
-
+// TYPES
+import { TextSectionProps } from '@/@types/TextSection';
 
 // TODO
 // I'd like to make this component handle another layer of object nesting so that it handles one Heading with zero or more Subheadings, each with one or more Paragraphs.
 
 export default function TextSection(props: TextSectionProps) {
-  const { heading, subheadings, paragraphs } = props.data
+  const { data, spaceParagraphs = true } = props
+  const { heading, paragraphs } = data
   return (
     <Box paddingBottom={2} >
       <Heading text={heading} align={AlignEnum.left} />
-      <Subheading text={heading} align={AlignEnum.left} />
       {
-        paragraphs.map((paragraph, index) => (
-          <Paragraph text={paragraph} key={index} />
-          // {
-          //   paragraphs.map((paragraph, index) => (
-          //     <Paragraph text={paragraph} key={index} />
-          //   ))
-          // }
+        paragraphs.map((paragraph: string, index) => (
+          <Paragraph text={paragraph} key={index} gutterBottom={spaceParagraphs} />
         ))
       }
     </Box>
