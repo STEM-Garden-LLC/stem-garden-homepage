5f6e51400268e44201bc4f5f5fed1dfd4d4df1c9
Nigel Wilson <nigel@stemgarden.org>
2024-01-22 16:24:14 -0600

    Update connectFourTypes.ts

--- src/pages/games/connect-four/connectFourTypes.ts
+++ src/pages/games/connect-four/connectFourTypes.ts
@@ -3,3 +3,17 @@ export enum ViewEnum {
   settings = 'settings',
   playGame = 'playGame',
 }
+
+export enum QuestionTypesEnum {
+  addTwoSingleDigitNumbers = "Add Single Digit Numbers",
+  addSingleDigitToDoubleDigit = "Add Single and Double Digit Numbers",
+  addTwoDoubleDigitNumbers = "Add Double Digit Numbers",
+}
+
+export enum TimeLimitTypesEnum {
+  noTimeLimit = "No Time Limit",
+  fiveSecondsPerQuestion = "Five Seconds per Question",
+  tenSecondsPerQuestion = "Ten Seconds per Question",
+  fiveMinutesForTheGame = "Five Minutes per Player for the Whole Game",
+  tenMinutesForTheGame = "Ten Minutes per Player for the Whole Game",
+}
