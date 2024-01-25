ef67f8d96d7979158cdbc5493cfa70e67894c9dc
Nigel Wilson <nigel@stemgarden.org>
2024-01-25 01:47:29 -0600

    Update ConnectFourContext.tsx

--- src/pages/games/connect-four/ConnectFourContext.tsx
+++ src/pages/games/connect-four/ConnectFourContext.tsx
@@ -5,32 +5,30 @@ import {
   getBoardData,
   getGameStatus,
   getLowestUnclaimedCells
- } from "./moveListHelpers";
+ } from "./helpers";
 
-import { testQuestion } from './questionGenerators/questionGenerator'
+// import { testQuestion } from './questionGenerators/questionGenerator'
+import { GameStatusEnum } from "../connect-four/connectFourTypes";
+
+function testQuestion() {
+  let vars = [1, 2, 3, 6]
+  return {
+      type: "missingSumThree",
+      vars: vars,
+      correctAnswer: vars[3],
+      equationString: `${vars[0]} + ${vars[1]} + ${vars[2]} = __`,
+      instructions: "Test Question",
+      inputType: "textField",
+  }
+}
 
 export const ConnectFourContext = createContext();
 // ConnectFourContext.displayName("Connect Four Context")
 
-export const ConnectFourContextProvider = props => {
-  const { maxSquareSideLength } = useContext(AppContext)
+export const ConnectFourContextProvider = (props: any) => {
+  // const { maxSquareSideLength } = useContext(AppContext)
 
-  const gameBoardConstants = {
-    oneSixth: '16.665%',
-    oneSeventh: '14.287%',
-    sixSevenths: '85.714%',
-    chipSizeRelativeToSquare: '84%',
-    rowNumbers: [0, 1, 2, 3, 4, 5],
-    columnNumbers: [0, 1, 2, 3, 4, 5, 6],
-    columnLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-    boardSideLength: (maxSquareSideLength - 96),
-    buttonContainerHeight: 96,
-    zIndex: {
-      chip: 100,
-      board: 200,
-    }
-
-  }
+  
 
   const defaultSettings = {
     opponent: 'human',
@@ -42,32 +40,34 @@ export const ConnectFourContextProvider = props => {
   // GAME State
   const [moveList, setMoveList] = useState([])  // The ids of the cells claimed in order with -1 indicating skipped turns
   
-  const [gameStatus, setGameStatus] = useState('playerOnesTurn')  // Enum ['playerOnesTurn', 'playerTwosTurn', 'playerOneWins', 'playerTwoWins', 'gameOverDraw'
-  const [boardData, setBoardData] = useState(new Array(42).fill('unclaimed')) // Indices correspond to cell ids, each value is one of "unclaimed" "playerOne" or "playerTwo"
-  const [nextPlayer, setNextPlayer] = useState('playerOne')
-  const [gameIsOver, setGameIsOver] = useState(false)
-  const [lowestUnclaimedCells, setLowestUnclaimedCells] = useState(gameBoardConstants.columnNumbers)
-  const [playerOnesMoves, setPlayerOnesMoves] = useState([])
-  const [playerTwosMoves, setPlayerTwosMoves] = useState([])
+  const [gameStatus, setGameStatus] = useState(GameStatusEnum.playerOnesTurn)
+  // const [boardData, setBoardData] = useState(new Array(42).fill('unclaimed')) // Indices correspond to cell ids, each value is one of "unclaimed" "playerOne" or "playerTwo"
+  // const [nextPlayer, setNextPlayer] = useState('playerOne')
+  // const [gameIsOver, setGameIsOver] = useState(false)
+  // const [lowestUnclaimedCells, setLowestUnclaimedCells] = useState(gameBoardConstants.columnNumbers)
+  // const [playerOnesMoves, setPlayerOnesMoves] = useState([])
+  // const [playerTwosMoves, setPlayerTwosMoves] = useState([])
 
-  const [openModal, setOpenModal] = useState("welcome") // Enum: "none", "question", "settings", "welcome"
+  // const [openModal, setOpenModal] = useState("welcome") // Enum: "none", "question", "settings", "welcome"
+  
+  // Active cell is the cell that would be added to the move list if the question is answered correctly.
   const [activeCell, setActiveCell] = useState(null) 
   const [question, setQuestion] = useState(testQuestion())
   const [headerText, setHeaderText] = useState("")
 
-  const [isBotsTurn, setIsBotsTurn] = useState(false)
+  // const [isBotsTurn, setIsBotsTurn] = useState(false)
 
   useEffect(() => {
     const updatedGameStatus = getGameStatus(moveList)
     setGameStatus(updatedGameStatus)
-    setBoardData(getBoardData(moveList))
-    setNextPlayer((moveList.length % 2 === 0) ? "playerOne" : "playerTwo")
-    setGameIsOver(['gameOverDraw','playerOneWins', 'playerTwoWins'].includes(updatedGameStatus))
+    // setBoardData(getBoardData(moveList))
+    // setNextPlayer((moveList.length % 2 === 0) ? "playerOne" : "playerTwo")
+    // setGameIsOver(['gameOverDraw','playerOneWins', 'playerTwoWins'].includes(updatedGameStatus))
 
-    setPlayerOnesMoves(moveList.filter((cell, turn) => turn % 2 === 0).filter(cell => cell !== -1))
-    setPlayerTwosMoves(moveList.filter((cell, turn) => turn % 2 === 1).filter(cell => cell !== -1))
+    // setPlayerOnesMoves(moveList.filter((cell, turn) => turn % 2 === 0).filter(cell => cell !== -1))
+    // setPlayerTwosMoves(moveList.filter((cell, turn) => turn % 2 === 1).filter(cell => cell !== -1))
 
-    setLowestUnclaimedCells(getLowestUnclaimedCells(moveList))
+    // setLowestUnclaimedCells(getLowestUnclaimedCells(moveList))
 
     setActiveCell(null) 
     setHeaderText("")
@@ -79,23 +79,23 @@ export const ConnectFourContextProvider = props => {
     moveList, setMoveList,
     
     gameStatus,
-    boardData,
-    nextPlayer,
-    gameIsOver,
-    playerOnesMoves,
-    playerTwosMoves,
+    // boardData,
+    // nextPlayer,
+    // gameIsOver,
+    // playerOnesMoves,
+    // playerTwosMoves,
 
-    lowestUnclaimedCells,
+    // lowestUnclaimedCells,
 
-    gameBoardConstants,
+    // gameBoardConstants,
 
 
-    openModal, setOpenModal,
+    // openModal, setOpenModal,
     activeCell, setActiveCell,
     question, setQuestion,
     headerText, setHeaderText,
 
-    isBotsTurn, setIsBotsTurn
+    // isBotsTurn, setIsBotsTurn
 
   }
 
